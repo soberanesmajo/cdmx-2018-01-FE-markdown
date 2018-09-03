edc9262fefbe679df6ed927c8cd9e2e6a552a5cd
soberanesmajo <soberanesmajo@gmail.com>
2018-09-03 15:36:53 -0500

    Estructura funcion 'readFileMd' y elimina errores en links

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-let fs = require('fs');
+const fs = require('fs');
 
-// Función que lee README 
+// Función para leer e identificar las URL's
 
-fs.readFile('./README.md', 'utf8', (err, data) => {
-  if (err) {
-    console.log('error: ', err);
-  } else {
-    // console.log(data);
-  }
-});
+const readFileMd = () => {
+  fs.readFile('./README.md', 'utf8', (err, data) => {
+    if (err) {
+      console.log('error: ', err);
+    } else {
+      // console.log(data);
+      let regEx = /((http:\/\/|https:\/\/|www\.)[^\s]+)/gim;
+      let newArray = data.match(regEx);
+      // console.log(newArray[0].slice(0, -1));
+      for (i = 0; i < newArray.length; i++) {
+        let newLink = newArray[i].slice(0, -1);
+        // console.log(newLink);
+        if (-1 === ',' || '.' || ';' || ':') {
+          newLink = newArray[i].slice(0, -2);
+          // console.log(newLink);
+        }
+      }
+    }
+  });
+};
 
-// Función para identificar las URL's
-
-fs.readFile('./README.md', 'utf8', (err, data) => {
-  if (err) {
-    console.log('error: ', err);
-  } else {
-    let regEx = /((http:\/\/|https:\/\/|www\.)[^\s]+)/gim;
-    let newArray = data.match(regEx);
-    console.log(newArray);
-  }
-});
+readFileMd();
